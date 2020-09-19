94f31ae7464a7a0c098a268259f0b0f875f8eddd
Deviur <deviur@yandex.ru>
2020-09-19 14:51:36 +0300

    Solution Topic 9 Lesson 6

--- anfisa4friends/settings.py
+++ anfisa4friends/settings.py
@@ -18,6 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Здесь укажите имя приложения, которое надо подключить
+    'homepage'
 ]
 
 MIDDLEWARE = [
