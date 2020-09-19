4be0103fc3820f199b6e4f9d3f62afa95557f721
Deviur <deviur@yandex.ru>
2020-09-19 19:33:45 +0300

    Solution Topic 10 Lesson 7

--- icecream/views.py
+++ icecream/views.py
@@ -5,9 +5,18 @@ from .models import icecream_db
 def icecream_list(request):
     icecreams = ''
     for i in range(len(icecream_db)):
-        # Исправьте строчку ниже
-        icecreams += f"{icecream_db[i]['name']} <br> "
-
-    context = {'icecreams': icecreams}
-    # Используйте render в return
+        icecreams += f"<a href='{i}/'>{icecream_db[i]['name']}</a><br>"
+    context = {
+        'icecreams': icecreams,
+    }
     return render(request, 'icecream/icecream-list.html', context)
+
+
+def icecream_detail(request, pk):
+    name = icecream_db[pk]['name']
+    description = icecream_db[pk]['description']
+    context = {
+        'name': name,
+        'description': description,
+    }
+    return render(request, 'icecream/icecream-detail.html', context)
